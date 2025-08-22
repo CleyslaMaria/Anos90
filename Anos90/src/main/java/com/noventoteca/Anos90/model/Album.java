8c6ea2f7030164d6c5bb968a93be46f3eae06aae
CleyslaMaria <168764227+CleyslaMaria@users.noreply.github.com>
2025-08-21 23:34:50 -0300

    Update Album.java

--- Anos90/src/main/java/com/noventoteca/Anos90/model/Album.java
+++ Anos90/src/main/java/com/noventoteca/Anos90/model/Album.java
@@ -16,4 +16,39 @@ public class Album extends Midia {
     public void setCodigo(String codigo){
         this.codigo = codigo;
     }
+
+    public static Album fromTxt(String registro){
+        String[] partes = registro.split(";");
+        if (partes.length < 7){
+        throw new IllegalArgumentException("Registro inválido: " + registro);
+        }
+        return new Album(
+            partes[0],
+            partes[1],
+            partes[2],
+            Integer.parseInt(partes[3]),
+            partes[4],
+            partes[5],
+            partes[6]
+        );
+    }
+
+    @Override
+    public String toTxt(){
+        return codigo + ";" + getTitulo() + ";" + getArtista() + ";" + getAno() + ";" + getGenero() + ";" + getLink() + ";" + getDescricao();
+    }
+
+    @Override
+    public String toString(){
+        return "Album{" +
+        "codigo='" + codigo +'\'' +
+        ", titulo='" + getTitulo() + '\'' +
+        ", artista='" + getArtista() + '\'' +
+        ", ano='" + getAno() + '\'' +
+        ", genero='" + getGenero() + '\'' +
+        ", link='" + getLink() + '\'' +
+        ", descricao='" + getDescricao() + '\'' +
+        '}';
+    }
+    
 }
