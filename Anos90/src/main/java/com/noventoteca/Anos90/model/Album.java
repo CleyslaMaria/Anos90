718b34749a06faa53a3597c2fc93ba3da4d83e4e
CleyslaMaria <168764227+CleyslaMaria@users.noreply.github.com>
2025-08-21 22:34:00 -0300

    Update Album.java

--- Anos90/src/main/java/com/noventoteca/Anos90/model/Album.java
+++ Anos90/src/main/java/com/noventoteca/Anos90/model/Album.java
@@ -1,5 +1,19 @@
 package com.noventoteca.Anos90.model;
 
-public class Album {
 
+public class Album extends Midia {
+    private String codigo;
+
+    public Album(String codigo, String titulo, String artista, int ano, String genero, String link, String descricao){
+        super(titulo, artista, ano, genero, link, descricao);
+        this.codigo = codigo;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
 }
