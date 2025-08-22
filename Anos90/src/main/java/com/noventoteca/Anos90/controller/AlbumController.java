f8e3152df9e5ee75014128b1e8e41c010da68a97
Maria-meedragon <aparecida.nascimento@dcomp.ufs.br>
2025-08-22 20:03:01 -0300

    Update AlbumController.java

--- Anos90/src/main/java/com/noventoteca/Anos90/controller/AlbumController.java
+++ Anos90/src/main/java/com/noventoteca/Anos90/controller/AlbumController.java
@@ -1,37 +1,6 @@
 package com.noventoteca.Anos90.controller;
 
-public class AlbumController {
-   private Long id;
-    private String titulo;
-    private String genero;
-    private String artista;
-    private int anoLancamento;
-
-    public Album() {}
-
-    public Album(Long id, String titulo, String genero, String artista, int anoLancamento) {
-        this.id = id;
-        this.titulo = titulo;
-        this.genero = genero;
-        this.artista = artista;
-        this.anoLancamento = anoLancamento;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getTitulo() { return titulo; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
-
-    public String getGenero() { return genero; }
-    public void setGenero(String genero) { this.genero = genero; }
-
-    public String getArtista() { return artista; }
-    public void setArtista(String artista) { this.artista = artista; }
-
-    public int getAnoLancamento() { return anoLancamento; }
-    public void setAnoLancamento(int anoLancamento) { this.anoLancamento = anoLancamento; }
-}
+import com.noventoteca.Anos90.model.Album;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -78,43 +47,6 @@ public class AlbumController {
         return albuns.removeIf(a -> a.getId().equals(id));
     }
 }
-
-
-import java.util.List;
-
-public class AlbumView {
-    private AlbumController controller;
-
-    public AlbumView(AlbumController controller) {
-        this.controller = controller;
-    }
-
-    public void exibirAlbuns(List<Album> albuns) {
-        System.out.println("<html><body>");
-        System.out.println("<h1>Lista de Álbuns</h1>");
-        System.out.println("<ul>");
-        for (Album a : albuns) {
-            System.out.printf("<li>%s - %s [%s] (%d)</li>%n",
-                    a.getTitulo(), a.getArtista(), a.getGenero(), a.getAnoLancamento());
-        }
-        System.out.println("</ul>");
-        System.out.println("</body></html>");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        AlbumController controller = new AlbumController();
-        AlbumView view = new AlbumView(controller);
-
-        // Lista inicial
-        view.exibirAlbuns(controller.listarTodos());
-
-        // Adiciona novo álbum
-        controller.adicionarAlbum("Ok Computer", "Alternative Rock", "Radiohead", 1997);
-
-        // Lista atualizada
-        view.exibirAlbuns(controller.listarTodos());
 
         // Busca por gênero
         System.out.println("=== Busca por gênero: Pop ===");
