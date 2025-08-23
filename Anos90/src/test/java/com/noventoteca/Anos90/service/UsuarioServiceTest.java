68e0634239e3ef9472cc7ed42c5d4d4678093006
Maria-meedragon <aparecida.nascimento@dcomp.ufs.br>
2025-08-23 20:48:10 -0300

    Update UsuarioServiceTest.java

--- Anos90/src/test/java/com/noventoteca/Anos90/service/UsuarioServiceTest.java
+++ Anos90/src/test/java/com/noventoteca/Anos90/service/UsuarioServiceTest.java
@@ -5,6 +5,13 @@ import com.noventoteca.Anos90.model.Usuario;
 import com.noventoteca.Anos90.model.UsuarioRepository;
 import com.noventoteca.Anos90.model.AlbumRepository;
 import com.noventoteca.Anos90.service.UsuarioService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
 
 public class UsuarioServiceTest {
 
