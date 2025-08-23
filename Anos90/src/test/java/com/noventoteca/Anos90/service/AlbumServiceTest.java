32dc849995e9970bf2c4fcd539f4598aeb6b058e
Maria-meedragon <aparecida.nascimento@dcomp.ufs.br>
2025-08-23 20:47:48 -0300

    Update AlbumServiceTest.java

--- Anos90/src/test/java/com/noventoteca/Anos90/service/AlbumServiceTest.java
+++ Anos90/src/test/java/com/noventoteca/Anos90/service/AlbumServiceTest.java
@@ -3,7 +3,14 @@ package com.noventoteca.Anos90.service;
 import com.noventoteca.Anos90.model.Album;
 import com.noventoteca.Anos90.model.AlbumRepository;
 import com.noventoteca.Anos90.service.AlbumService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
 
 public class AlbumServiceTest {
+  
 
 }
