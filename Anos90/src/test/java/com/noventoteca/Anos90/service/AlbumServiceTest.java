1ec1752ffe3b85a508fdd9ddaa1cecb1309341df
Maria-meedragon <aparecida.nascimento@dcomp.ufs.br>
2025-08-23 23:28:07 -0300

    Update AlbumServiceTest.java

--- Anos90/src/test/java/com/noventoteca/Anos90/service/AlbumServiceTest.java
+++ Anos90/src/test/java/com/noventoteca/Anos90/service/AlbumServiceTest.java
@@ -10,7 +10,37 @@ import static org.mockito.Mockito.*;
 
 import java.util.List;
 
-public class AlbumServiceTest {
-  
+class AlbumServiceTest {
 
+    private AlbumRepository albumRepository;
+    private AlbumService albumService;
+
+    @BeforeEach
+    void setUp() {
+        albumRepository = mock(AlbumRepository.class);
+        albumService = new AlbumService(albumRepository);
+    }
+
+    @Test
+    void deveListarAlbuns() {
+        Album album = new Album("A001", "Ok Computer", "Radiohead", 1997, "Alternative Rock", "link1", "desc");
+        when(albumRepository.listarAlbuns()).thenReturn(List.of(album));
+
+        List<Album> resultado = albumService.listarAlbuns();
+
+        assertEquals(1, resultado.size());
+        assertEquals("Ok Computer", resultado.get(0).getTitulo());
+        verify(albumRepository, times(1)).listarAlbuns();
+    }
+
+    @Test
+    void deveBuscarPorCodigo() {
+        Album album = new Album("A001", "Ok Computer", "Radiohead", 1997, "Alternative Rock", "link1", "desc");
+        when(albumRepository.buscarPorCodigo("A001")).thenReturn(album);
+
+        Album resultado = albumService.burcarPorCodigo("A001");
+
+        assertNotNull(resultado);
+        assertEquals("Radiohead", resultado.getArtista());
+    }
 }
